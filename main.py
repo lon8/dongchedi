699aa10ce6a38baaf798b7348e9c428265ea4716
lon8 <vlad.kanatik@ya.ru>
2023-09-19 08:55:16 +0000

    parse html

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+response = requests.get('https://www.dongchedi.com/auto/series/690')
+
+with open('index.html', 'w', encoding='utf-8') as file:
+    json.dump(response.text, file, indent=4, ensure_ascii=False)
+
+print(response.text)
